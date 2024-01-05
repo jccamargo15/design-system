7b27f886e8b44fcb2c2e9aafb54a2e82b25e03c4
José Carlos de Camargo <jccamargo15@gmail.com>
2024-01-04 21:45:33 -0300

    feat: Componentes do Design System - Componente: Text

--- packages/docs/src/stories/Text.stories.tsx
+++ packages/docs/src/stories/Text.stories.tsx
@@ -4,10 +4,17 @@ import { Text, TextProps } from '@ignite-ui/react'
 export default {
   title: 'Typography/Text',
   component: Text,
-
   args: {
-    children: 'Exemple test',
+    children:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione cumque aut qui veritatis in, provident rerum fuga. Vero nam dolore officia optio eveniet fuga autem, deleniti nostrum non incidunt eos?',
   },
 } as Meta<TextProps>
 
 export const Primary: StoryObj<TextProps> = {}
+
+export const CustomTag: StoryObj<TextProps> = {
+  args: {
+    children: 'Strong text',
+    as: 'strong',
+  },
+}
