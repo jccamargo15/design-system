ade589ea1645e625768098f4f0360fe8d6eb93d5
José Carlos de Camargo <jccamargo15@gmail.com>
2024-01-09 20:55:33 -0300

    feat: Melhorias no Storybook - Controles nos componentes

--- packages/docs/src/stories/Avatar.stories.tsx
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -8,6 +8,13 @@ export default {
     src: 'https://github.com/jccamargo15.png',
     alt: 'José Camargo',
   },
+  argTypes: {
+    src: {
+      control: {
+        type: 'text',
+      },
+    },
+  },
 } as Meta<AvatarProps>
 
 export const Primary: StoryObj<AvatarProps> = {}
--- packages/docs/src/stories/Box.stories.tsx
+++ packages/docs/src/stories/Box.stories.tsx
@@ -4,13 +4,17 @@ import { Box, BoxProps, Text } from '@ignite-ui/react'
 export default {
   title: 'Surfaces/Box',
   component: Box,
-
   args: {
     children: (
       <>
         <Text>Testando o elemento Box.</Text>
       </>
     ),
+  },
+  argTypes: {
+    children: {
+      control: { type: null },
+    },
   },
 } as Meta<BoxProps>
 
